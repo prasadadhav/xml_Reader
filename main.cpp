86964fdfd165216268890b4fcf1ecc9b53770798
padhav <prasad.adhav@uni.lu>
2021-07-14 12:12:22 +0200

    created files

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <xml_reader.h>
+
+// includes for Boost
+#include<boost/property_tree/ptree.hpp>
+using boost::property_tree::ptree;
+#include<boost/property_tree/xml_parser.hpp>
+#include<boost/foreach.hpp>
+
+int main()
+{
+    ptree pt;
+
+    read
+}
---
+++ xml_reader.h
@@ -0,0 +1,18 @@
+#ifndef XML_READER_H
+#define XML_READER_H
+
+#include<iostream>
+
+// includes for Boost
+#include<boost/property_tree/ptree.hpp>
+using boost::property_tree::ptree;
+#include<boost/property_tree/xml_parser.hpp>
+#include<boost/foreach.hpp>
+
+class xml_reader
+{
+public:
+    xml_reader();
+};
+
+#endif // XML_READER_H
