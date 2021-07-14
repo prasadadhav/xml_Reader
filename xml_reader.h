b605a60602987008faace85a40cd9401343c56db
padhav <prasad.adhav@uni.lu>
2021-07-14 14:05:48 +0200

    Declarations of xml reader class member functions

--- xml_reader.h
+++ xml_reader.h
@@ -14,6 +14,23 @@ class xml_reader
 public:
     // constructor
     xml_reader();
+
+    // destructor
+    ~xml_reader();
+
+    // Create an empty property tree object
+    boost::property_tree::ptree my_ptree_obj;
+
+private:
+    // get file name
+    int get_fileName(const std::string filename);
+
+public:
+    // set xml attributes
+    void set_xmlAttributes();
+
+    // print out xml attributes
+    void print_xmlAttributes();
 };
 
 #endif // XML_READER_H
