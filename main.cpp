f88bc7fe7be58ca19f5f57ba37a3ef57f08388ba
padhav <prasad.adhav@uni.lu>
2021-07-14 13:14:31 +0200

    working version

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-#include <xml_reader.h>
+#include "xml_reader.h"
 
 // includes for Boost
 #include<boost/property_tree/ptree.hpp>
@@ -6,9 +6,49 @@ using boost::property_tree::ptree;
 #include<boost/property_tree/xml_parser.hpp>
 #include<boost/foreach.hpp>
 
-int main()
+using namespace std;
+
+
+int main(int argc, char** argv)
+//int main()
 {
+    // Check parameters
+    if (argc <= 1)
+    {
+        std::cout << "Error: Invalid parameters!" << std::endl;
+        std::cout << "Usage ./xml_Reader_test_project xmlFile" << std::endl;
+        std::cout << std::endl;
+        std::cout << "Parameter description" << std::endl;
+        std::cout << "  configurationFile: Path and filename of xml file" << std::endl;
+        return 1;
+    }
+    // Remove the first parameter
+    argc--;
+
+//    // First parameter must the xml file
+    const std::string filename(argv[1]);
+    if (filename.length() == 0)
+    {
+        std::cout << "Error: No file name and path provided!" << std::endl;
+        return 0;
+    }
+
+    // Create an empty property tree object
     ptree pt;
 
-    read
+    // Load the XML file into the property tree. If reading fails
+    // (cannot open file, parse error), an exception is thrown.
+//    read_xml(filename, pt);
+    read_xml(argv[1], pt);
+
+    BOOST_FOREACH(ptree::value_type & child, pt.get_child("localinfo.player_info"))
+    {
+        cout << "Node name: " << child.first << endl;
+        cout << "Pass value: " << child.second.get<string>("<xmlattr>.pass") << endl;
+        cout << "isSave value: " << child.second.get<int>("<xmlattr>.isSave") << endl;
+        cout << "isLastLogin value: " << child.second.get<int>("<xmlattr>.isLastLogin") << endl;
+        cout << "account value: " << child.second.get<string>("<xmlattr>.account") << endl;
+    }
+
+    return 0;
 }
