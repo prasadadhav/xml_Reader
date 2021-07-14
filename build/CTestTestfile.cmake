acfca39d66e293598adbe70a5fe86db1a6fc0452
padhav <prasad.adhav@uni.lu>
2021-07-14 15:01:35 +0200

    clean up and build update

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/prasad/09_personal/0_CPP/2_XML_Parser/xml_Reader_test_project
-# Build directory: /home/prasad/09_personal/0_CPP/2_XML_Parser/xml_Reader_test_project/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
